737be86745e059be15b81fa15344c2d23aa76aac
Vishwa <vishwa20@outlook.com>
2020-08-02 23:46:38 +0530

    Fixed the firebase code

--- much_faster_model/firebase.py
+++ much_faster_model/firebase.py
@@ -5,42 +5,33 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 import googlemaps
-from time import time
-
+import numpy as np
 # Fetch the service account key JSON file contents
 try:
-    cred = credentials.Certificate('../smart-waste-locator-firebase-adminsdk-ljjzx-495a7e327a.json')
+    cred = credentials.Certificate('../../smart-waste-locator-firebase-adminsdk-ljjzx-495a7e327a.json')
     # Initialize the app with a service account, granting admin privileges
     firebase_admin.initialize_app(cred, {
         'databaseURL': 'https://smart-waste-locator.firebaseio.com/'
     })
 except:
     print("Already Initialized")
-
-with open("../appkey","r") as f:
-    appkey = f.read()[:-1]
-
-gmaps_keys = googlemaps.Client(key = appkey)
+    
+gmaps_keys = googlemaps.Client(key = "AIzaSyDeuccZuXIp4Ncemlzgqs8YoKfg3xixJ-c")
     
 
 def cloud_upload_image(image):
     try:
-        os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="../smart-waste-locator-firebase-adminsdk-ljjzx-495a7e327a.json"
+        os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="../../smart-waste-locator-firebase-adminsdk-ljjzx-495a7e327a.json"
         # Enable firebase Storage
         client = storage.Client()
         # Reference an existing bucket.
         bucket = client.get_bucket('smart-waste-locator.appspot.com')
-        # Upload a local file to a new file to be created in your bucket.
-        imname = str(time())+'.jpg'
-        # path = "./"+imname
-        Blob = bucket.blob(imname)
-        encoded, enimg = cv2.imencode('.jpg', image)
-        if not Blob.exists():
-            Blob.upload_from_string(enimg.tobytes(), content_type='image/jpeg')
-            print("Image Uploaded")
-        # Blob.upload_from_filename(filename=path)
+        # Upload a local file to a new file to be created in your bucket. 
+        path = "./"+image
+        Blob = bucket.blob(image)
+        Blob.upload_from_filename(filename=path)
         #returning the url
-        return 'gs://smart-waste-locator.appspot.com/'+imname
+        return 'https://firebasestorage.googleapis.com/v0/b/smart-waste-locator.appspot.com/o/'+image+'?alt=media'
     except:
         return None
     
@@ -59,6 +50,7 @@ def add_data(image,latitude,longitude):
         child.set(
             {
                 "Cleaned":"False",
+                "Notified": False,
                 "TimeStamp":date,
                 "image":link,
                 "Area":area,
@@ -74,3 +66,10 @@ def add_data(image,latitude,longitude):
 def GenerateRandomCoordinates():
     #Generate Random Coordiantes in Pune
     return [float("%1.4f"%(np.random.uniform(18.4311,18.5995))), float("%1.4f"%(np.random.uniform(73.7469,73.9474)))]
+
+
+if __name__ == "__main__":
+    image = "../../img_waste.jpg"
+    coords = GenerateRandomCoordinates()
+    print(coords)
+    print(add_data(image,coords[0],coords[1]))
