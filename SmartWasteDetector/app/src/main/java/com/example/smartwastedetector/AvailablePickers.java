23d1146e999ffb405eb21352358d2cbc634cebad
Abhishek Wahane <abhishekwahane@icloud.com>
2020-08-03 14:17:33 +0530

    Waste index on each pincode displayed along with task assignment

--- SmartWasteDetector/app/src/main/java/com/example/smartwastedetector/AvailablePickers.java
+++ SmartWasteDetector/app/src/main/java/com/example/smartwastedetector/AvailablePickers.java
@@ -205,14 +205,15 @@ public class AvailablePickers extends AppCompatActivity {
                     @Override
                     public void onClick(View v) {
 
-                        for(String id: garbageLocationIds){
+                        for(int i =0;i< garbageLocationIds.size();i++){
 
                             Log.d("GAR_ID",pickersIds.get(position));
                             final HashMap<String, Object> updatePickerID = new HashMap<String, Object>();
                             updatePickerID.put("collectorid",pickersIds.get(position));
-                            ref.child("Detected").child(id).updateChildren(updatePickerID);
+                            ref.child("Detected").child(garbageLocationIds.get(i)).updateChildren(updatePickerID);
 
-                            task newTask = new task(latitude,longitude,false);
+
+                            task newTask = new task(latitudes.get(i),longitudes.get(i),false);
                             final String uniqueID = UUID.randomUUID().toString();
                             String taskID = (uniqueID.substring(uniqueID.length()-8));
 
